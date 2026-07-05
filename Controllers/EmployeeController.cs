85709018a9bf8ceff1a2174a77c2baf52ed4942c
Aditya Sonone <adityasonone580@gmail.com>
2026-07-05 17:00:59 +0530

    Refactor controller and improve resource management by adding using keyword in SQL connection

--- Controllers/EmployeeController.cs
+++ Controllers/EmployeeController.cs
@@ -1,19 +1,23 @@
 ﻿using ASPCoreWebAPI.Models;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
+using Newtonsoft.Json; //it is no longer needed, as we are not using Json.Serialize, Ok() uses System.Text.Json for serialization
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
 
 namespace ASPCoreWebAPI.Controllers
 {
-    [Route("api/Employee")]
+    [Route("api/[controller]")]
     [ApiController]
-    public class StudentController : Controller
+
+    //since we are using webapi, we don't need whole Controller, since WEBAPI never returns View()
+    //so, we are using ControllerBase, as we don't need additional MVC views functionality.
+    //Controller = ControllerBase + MVC View support.
+    public class EmployeeController : ControllerBase
     {
 
         public readonly IConfiguration _configuration;
-        public StudentController(IConfiguration configuration)
+        public EmployeeController(IConfiguration configuration)
         {
             _configuration = configuration;
         }
@@ -22,14 +26,14 @@ namespace ASPCoreWebAPI.Controllers
         //[Obsolete]
         public ActionResult<List<Employee>> GetEmployees()
         {
-            SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("EmployeeCon").ToString());
-            SqlDataAdapter sqlData = new SqlDataAdapter("Select * FROM EmployeeInfo",sqlConnection);
+            using SqlConnection sqlConnection = new SqlConnection(_configuration.GetConnectionString("EmployeeCon").ToString()); //using keyword gaurantees that Dispose() will be called even if sql connection failed!! So, the database can be accissible for others also.
+            using SqlDataAdapter sqlData = new SqlDataAdapter("Select * FROM EmployeeInfo",sqlConnection);
             DataTable dt = new DataTable();
             sqlData.Fill(dt);
 
 
             List<Employee> empList = new List<Employee>();
-            Response response = new Response();
+            //Response response = new Response(); //using this for editing response messages, but now we are using NotFound()
 
             if (dt.Rows.Count > 0)
             {
@@ -45,12 +49,13 @@ namespace ASPCoreWebAPI.Controllers
             }
             if (empList.Count > 0)
             {
-                return Ok(empList);
+                return Ok(empList); //creates object of OkObjectResult, which will give 200Ok statusCode, and uses System.Text.Json for serialization
             }
             else
             {
                 return NotFound("No data found!!");
             }
+
             //return View();
         }
     }
